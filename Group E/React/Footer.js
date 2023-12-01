501b5e0fb90246623595f851c59d8ca9017dd660
theofhevs <theo.falcinelli@students.hevs.ch>
2023-12-01 09:47:53 +0100

    First look on the usage of React

---
+++ Group E/React/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <img id="logo" src="ressources/images/logo.png" />
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ Group E/React/script-react.js
@@ -0,0 +1,6 @@
+// script-react.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MockupPage from './intro';
+
+ReactDOM.render(<MockupPage />, document.getElementById('app'));
